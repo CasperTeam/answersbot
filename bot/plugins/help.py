6c732d4c665522cd318c4e9cb3f038a5c3126ae5
Casperteam <84174959+CasperTeam@users.noreply.github.com>
2022-02-17 22:25:32 +0530

    Update help.py

--- bot/plugins/help.py
+++ bot/plugins/help.py
@@ -28,6 +28,6 @@ async def sheet(client: Bot, message: Message):
     "New Features Will Come bu for next batch."
     "ℹ️ Thanks 😍 for using this bot❗️❣️"
     )  
-    await message.reply_text(DEFAULT_START_TEXT, reply_markup="md")
+    await message.reply_text(DEFAULT_START_TEXT)
 
     
