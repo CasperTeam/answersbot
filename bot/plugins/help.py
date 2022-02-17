d305b37f6208725904a65f5fc3e0e7f753c983ed
Casperteam <84174959+CasperTeam@users.noreply.github.com>
2022-02-17 22:23:08 +0530

    Update help.py

--- bot/plugins/help.py
+++ bot/plugins/help.py
@@ -12,3 +12,22 @@ async def sheet(client: Bot, message: Message):
     await message.reply_text("https://docs.google.com/spreadsheets/d/1Iuz971A-HmOQBf3y005XBO3XCEpsJZly2GL8Cc6Gar0/edit?usp=drivesdk")
 
     
+@Bot.on_message(
+    filters.command("start")
+)
+async def sheet(client: Bot, message: Message):
+    DEFAULT_START_TEXT = (
+    "Hi. ☺️\n"
+    "Thank you for using me.\n\n"
+    "This is abot to contact admins! \n\n\n"
+    "Google Sheets Link -> /sheet \n\n\n"
+    "**Rules for using this bot:** "
+    "1. Ask your question Directly "
+    "2. dont edit or delete messages "
+    "3. **Dont Spam!** \n\n\n"
+    "New Features Will Come bu for next batch."
+    "ℹ️ Subscribe @t24jeetalk 😍 for using this bot❗️❣️"
+    )  
+    await message.reply_text(DEFAULT_START_TEXT, reply_markup="md")
+
+    
