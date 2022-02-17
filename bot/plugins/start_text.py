85eb7d5c352d576056c7c312787ba5b10e5d7cb4
Casperteam <84174959+CasperTeam@users.noreply.github.com>
2022-02-17 22:08:50 +0530

    Update start_text.py

--- bot/plugins/start_text.py
+++ bot/plugins/start_text.py
@@ -20,25 +20,22 @@ from bot.hf.flifi import uszkhvis_chats_ahndler
 
 
 @Bot.on_message(
-    filters.command(START_COMMAND, COMMM_AND_PRE_FIX) &
-    ~uszkhvis_chats_ahndler([AUTH_CHANNEL])
+    filters.command(START_COMMAND, COMMM_AND_PRE_FIX)
 )
-async def num_start_message(client: Bot, message: Message):
-    await message.reply_text(
-        client.commandi[START_COMMAND],
-        quote=True
-    )
-
-
-@Bot.on_message(
-    filters.command(START_COMMAND, COMMM_AND_PRE_FIX) &
-    uszkhvis_chats_ahndler([AUTH_CHANNEL])
+DEFAULT_START_TEXT = (
+    "Hi. ☺️\n"
+    "Thank you for using me.\n\n"
+    "This is abot to contact admins! \n\n\n"
+    "Google Sheets Link -> /sheet \n\n\n"
+    "**Rules for using this bot:** "
+    "1. Ask your question Directly "
+    "2. dont edit or delete messages "
+    "3. **Dont Spam!** \n\n\n"
+    "New Features Will Come bu for next batch."
+    "ℹ️ Subscribe @t24jeetalk 😍 for using this bot❗️❣️"
 )
-async def nimda_start_message(_, message: Message):
-    await message.reply_text(
-        ONLINE_CHECK_START_TEXT,
-        quote=True
-    )
+async def nimda_start_message(c, m):
+    await m.reply_text(DEFAULT_START_TEXT)
 
 @Bot.on_message(
     filters.command(HELP_COMMAND, COMMM_AND_PRE_FIX) &
